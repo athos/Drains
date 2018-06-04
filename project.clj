28de2c4c43cfcec65a0811b784518079294f825d
Shogo Ohta <athos0220@gmail.com>
2018-06-05 00:25:35 +0900

    Update project.clj

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject drains "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "A tiny, but incredibly flexible library that utilizes reducing fns and transducers to build composable and reusable computations."
+  :url "https://github.com/athos/Drains"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]])
+  :dependencies [[org.clojure/clojure "1.9.0"]])
