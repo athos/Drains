427454d9d162192ee74aff4a60dd7d476208d384
Shogo Ohta <athos0220@gmail.com>
2018-06-05 15:31:41 +0900

    Wrap drains with fns to make them reusable safely

--- src/drains/core.clj
+++ src/drains/core.clj
@@ -13,61 +13,67 @@
   (-residue [this] val)
   (-attach [this _] this))
 
+(defn- unwrap [x]
+  (if (fn? x) (x) x))
+
 (defn drain
   ([rf] (drain rf (rf)))
   ([rf init] (drain nil rf init))
   ([xform rf init]
-   (let [rf (cond-> rf xform xform)
-         val (volatile! init)]
-     (reify IDrain
-       (-flush [this input]
-         (let [val' (rf @val input)]
-           (if (reduced? val')
-             (->ReducedDrain (rf @val'))
-             (do (vreset! val val')
-                 this))))
-       (-residue [this] @val)
-       (-attach [this xf]
-         (drain xf rf @val))))))
+   (fn []
+     (let [rf (cond-> rf xform xform)
+           val (volatile! init)]
+       (reify IDrain
+         (-flush [this input]
+           (let [val' (rf @val input)]
+             (if (reduced? val')
+               (->ReducedDrain (rf @val'))
+               (do (vreset! val val')
+                   this))))
+         (-residue [this] @val)
+         (-attach [this xf]
+           (drain xf rf @val)))))))
 
 (defn drains [ds]
-  (let [ds (volatile! (cond-> ds (seq? ds) vec))
-        map' (fn [f ds]
-               (->> ds
-                    (reduce-kv #(assoc! %1 %2 (f %3)) (transient ds))
-                    persistent!))]
-    (reify IDrain
-      (-flush [this input]
-        (vreset! ds
-                 (reduce-kv (fn [ds k drain]
-                              (let [drain' (-flush drain input)]
-                                (if (identical? drain drain')
-                                  ds
-                                  (assoc ds k drain'))))
-                            @ds
-                            @ds))
-        this)
-      (-residue [this]
-        (map' -residue @ds))
-      (-attach [this xf]
-        (drains (map' #(-attach % xf) @ds))))))
+  (fn []
+    (let [map' (fn [f ds]
+                 (->> ds
+                      (reduce-kv #(assoc! %1 %2 (f %3)) (transient ds))
+                      persistent!))
+          ds (volatile! (map' unwrap (cond-> ds (seq? ds) vec)))]
+      (reify IDrain
+        (-flush [this input]
+          (vreset! ds
+                   (reduce-kv (fn [ds k drain]
+                                (let [drain' (-flush drain input)]
+                                  (if (identical? drain drain')
+                                    ds
+                                    (assoc ds k drain'))))
+                              @ds
+                              @ds))
+          this)
+        (-residue [this]
+          (map' -residue @ds))
+        (-attach [this xf]
+          (drains (map' #(-attach % xf) @ds)))))))
 
 (defn fmap [f d]
-  (let [d (volatile! d)]
-    (reify IDrain
-      (-flush [this input]
-        (vswap! d -flush input)
-        this)
-      (-residue [this]
-        (f (-residue @d)))
-      (-attach [this xf]
-        (fmap f (-attach @d xf))))))
+  (fn []
+    (let [d (volatile! (unwrap d))]
+      (reify IDrain
+        (-flush [this input]
+          (vswap! d -flush input)
+          this)
+        (-residue [this]
+          (f (-residue @d)))
+        (-attach [this xf]
+          (fmap f (-attach @d xf)))))))
 
 (defn combine-with [f & ds]
   (fmap #(apply f %) (drains ds)))
 
 (defn with [xf d]
-  (-attach d xf))
+  (-attach (unwrap d) xf))
 
 (defn into [drain xs]
-  (-residue (cc/reduce -flush drain xs)))
+  (-residue (cc/reduce -flush (unwrap drain) xs)))
