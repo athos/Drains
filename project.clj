126229536307f181e4bf05b6c0afa0b550965f3c
Shogo Ohta <athos0220@gmail.com>
2018-06-07 22:44:38 +0900

    Add CLJS to project.clj deps

--- project.clj
+++ project.clj
@@ -3,4 +3,5 @@
   :url "https://github.com/athos/Drains"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.9.0"]])
+  :dependencies [[org.clojure/clojure "1.9.0"]
+                 [org.clojure/clojurescript "1.10.238"]])
