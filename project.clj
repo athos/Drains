a5def29f07425e8be811334541d673c9cd44a3f1
Shogo Ohta <athos0220@gmail.com>
2018-06-07 22:41:54 +0900

    Update project description

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject drains "0.1.0-SNAPSHOT"
-  :description "A tiny, but incredibly flexible library that utilizes reducing fns and transducers to build composable and reusable computations."
+  :description "Swiss Army knife for sequence data aggregation in Clojure(Script)"
   :url "https://github.com/athos/Drains"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
